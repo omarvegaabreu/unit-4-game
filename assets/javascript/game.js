ce6166457cf5cb878b90dd0804451cfce890c7ae
omarvegaabreu <39449747+omarvegaabreu@users.noreply.github.com>
2019-06-17 13:47:23 -0400

    js logic is done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 //The player will be shown a random number at the start of the game. This number should be
 // between 19 - 120. random_results variable will have the values.
+// random numbers are attached the the global variables in order for all functions to have access.
 var random_result;
 var lost = 0;
 var win = 0;
 var previous = 0;
 
 var resetAndStartGame = function() {
-  $(".crystals").empty();
+  $(".crystals").empty(); //Will hide this amount until the player clicks a crystal.
 
   random_result = Math.floor(Math.random() * (120 - 19) + 19);
   $("#result").html("Random Result: " + random_result);
@@ -14,21 +15,29 @@ var resetAndStartGame = function() {
   for (var i = 0; i < 4; i++) {
     //Each crystal should have a random hidden value between 1 - 12.
     var random = Math.floor(Math.random() * 12) + 1;
-
+    //variable to add div to the crystals loop
     var crystal = $("<div>");
+
+    //This object adds attributes in the html taking the random variable
     crystal.attr({
       class: "crystal",
       "data-random": random
     });
 
+    //This method assignees a random number to the image in the html.
     crystal.html(random);
 
+    //jquery functions to append the crystal variable which contains the <divs> to the html.
     $(".crystals").append(crystal);
   }
 };
 
+//function call which restarts game
 resetAndStartGame();
 
+//When the player clicks on a crystal,
+//When the player clicks one crystal, it will dynamically update the player's score counter.
+//it will add a specific amount of points to the player's total score.
 $(document).on("click", ".crystal", function() {
   var num = parseInt($(this).attr("data-random"));
 
@@ -38,31 +47,19 @@ $(document).on("click", ".crystal", function() {
 
   if (previous > random_result) {
     lost--;
-    $("#lost").html(lost);
 
+    $("#lost").html(lost);
+    //The player loses if their score goes above the random number.
     previous = 0;
-    resetAndStartGame();
+
+    resetAndStartGame(); //The game restarts whenever the player wins or loses.
   } else if (previous === random_result) {
     win++;
 
     $("#win").html(win);
 
     previous = 0;
-    resetAndStartGame();
+
+    resetAndStartGame(); //The game restarts whenever the player wins or loses.
   }
 });
-
-//When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-
-//The game will hide this amount until the player clicks a crystal.
-//When the player clicks one crystal, it will dynamically update the player's score counter.
-
-//The player wins if their total score matches the random number from the beginning of the game.
-
-//The player loses if their score goes above the random number.
-
-//The game restarts whenever the player wins or loses.
-
-//When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
